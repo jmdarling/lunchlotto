236a219bd916873f023d027df18fae2817c8b665
Jonathan Darling <jmdarling@me.com>
2016-08-04 01:15:00 -0500

    Enable CORS

--- app.js
+++ app.js
@@ -11,6 +11,12 @@ const LunchCrew = require('./models/LunchCrew')
 
 const app = express()
 
+app.use((request, response, next) => {
+  response.header('Access-Control-Allow-Origin', '*')
+  response.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+  next()
+})
+
 app.use(bodyParser.json())
 app.use(morgan('dev'))
 
